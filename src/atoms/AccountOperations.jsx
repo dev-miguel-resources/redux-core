85fe1e1caa6bcd4ccf21d62bda30249385331a53
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-09-20 23:55:45 -0300

    first steps in bank app with redux core

---
+++ src/atoms/AccountOperations.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+
+const AccountOperations = () => {
+  const [depositAmount, setDepositAmount] = useState('')
+  const [withdrawalAmount, setWithdrawalAmount] = useState('')
+  const [loanAmount, setLoanAmount] = useState('')
+  const [loanPurpose, setLoanPurpose] = useState('')
+  const [currency, setCurrency] = useState('USD')
+
+  function handleDeposit() {}
+  function handleWithdrawal() {}
+  function handleRequestLoan() {}
+  function handlePayLoan() {}
+
+  return (
+    <>
+      <h2>Your account operations</h2>
+      <div className="inputs">
+        <div>
+          <label>Deposit</label>
+          <input
+            type="number"
+            value={depositAmount}
+            onChange={(e) => setDepositAmount(+e.target.value)}
+          />
+          <select
+            value={currency}
+            onChange={(e) => setCurrency(e.target.value)}
+          >
+            <option value="USD">US Dollar</option>
+            <option value="EUR">Euro</option>
+            <option value="GBP">British Pound</option>
+          </select>
+
+          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
+        </div>
+
+        <div>
+          <label>Withdraw</label>
+          <input
+            type="number"
+            value={withdrawalAmount}
+            onChange={(e) => setWithdrawalAmount(+e.target.value)}
+          />
+          <button onClick={handleWithdrawal}>
+            Withdraw {withdrawalAmount}
+          </button>
+        </div>
+
+        <div>
+          <label>Request Loan</label>
+          <input
+            type="number"
+            value={loanAmount}
+            onChange={(e) => setLoanAmount(+e.target.value)}
+            placeholder="Loan Amount"
+          />
+          <input
+            value={loanPurpose}
+            onChange={(e) => setLoanPurpose(e.target.value)}
+            placeholder="Loan purpose"
+          />
+          <button onClick={handleRequestLoan}>Request Loan</button>
+        </div>
+
+        <div>
+          <span>Pay back $x</span>
+          <button onClick={handlePayLoan}>Pay loan</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default AccountOperations
---
+++ src/atoms/Customer.jsx
@@ -0,0 +1,3 @@
+const Customer = () => <h2>Welcome, %NAME%</h2>
+
+export default Customer
